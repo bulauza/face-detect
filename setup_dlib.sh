e1b3a23e51e4afe7e30e56d80f9a71fd49788af2
bulauza <bulauza@gmail.com>
2019-07-04 13:55:24 +0900

    modified setup file

--- setup_dlib.sh
+++ setup_dlib.sh
@@ -5,9 +5,9 @@ echo $(colored $cyan "Start")
 check pip3 install -r requirements.txt
 
 # Download landmarks detect file
-#if [ ! -e "./shape_predictor_68_face_landmarks.dat" ]; then
-#    check wget http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2
-#    check bzip2 -d shape_predictor_68_face_landmarks.dat.bz2
-#fi
+if [ ! -e "./shape_predictor_68_face_landmarks.dat" ]; then
+    check wget http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2
+    check bzip2 -d shape_predictor_68_face_landmarks.dat.bz2
+fi
 
 echo $(colored $cyan "Finished")
